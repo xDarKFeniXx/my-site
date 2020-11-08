482d6ea509abe4afff656d0254f73bc7c611e10b
xDarKFeniXx <dark.fenix.tula@gmail.com>
2020-11-09 01:05:14 +0300

    add styles for trminal and start script

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
-console.log('hello');
+const userText = [
+    "ok, I've started",
+    "I need finish this",
+    "fixed padding",
+    "I hate make up a website"
+]
+window.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+    const userTextSpan = document.querySelector('.Prompt__text')
+
+
+    userText.forEach((text, index) => {
+
+        setTimeout(() => {
+            userTextSpan.textContent = text
+        }, index * 2000)
+    })
+})
