b69991eed186422c0c4030475a67227427e067ba
xDarKFeniXx <dark.fenix.tula@gmail.com>
2020-11-21 00:06:32 +0300

    add skills block

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,20 @@
-import '@fortawesome/fontawesome-free';
 
-const userText = [
-    "ok, I've started",
-    "I need finish this",
-    "fixed padding",
-    "I hate make up a website"
-]
+
 const scenaries=[
     {id:1, text: "yarn add", isUser: true, clearPrev: false},
-    {id:2, text: "...", isUser: false, clearPrev: false},
-    {id:3, text: "succes", isUser: false, clearPrev: true},
+    {id:2, text: "System: ...", isUser: false, clearPrev: false},
+    {id:3, text: "System: succes", isUser: false, clearPrev: true},
     {id:4, text: "bad padding", isUser: true, clearPrev: true},
     {id:5, text: "I'm fixing it", isUser: true, clearPrev: true},
-    {id:6, text: "ok, i move all blocks in space", isUser: false, clearPrev: true},
+    {id:6, text: "System: ok, i move all blocks in space", isUser: false, clearPrev: true},
     {id:7, text: "...", isUser: true, clearPrev: false},
     {id:8, text: "fixed it", isUser: true, clearPrev: true},
 ]
 const showDate=()=>{
     const dateEl=document.querySelector('.header__date')
     const currentDate=new Date()
-    dateEl.textContent=currentDate.toLocaleString('ru',
-    {weekday:"long", hour:"2-digit", minute: "2-digit"}
+    dateEl.textContent=currentDate.toLocaleString('en',
+    {weekday:"long", hour:"2-digit", minute: "2-digit", hour12:false}
     );
 }
 
@@ -32,7 +26,6 @@ window.addEventListener('DOMContentLoaded', () => {
     const systemTextSpan=document.querySelector('.Terminal__text')
 
     scenaries.forEach((item, index) => {
-        console.log(item);
         setTimeout(() => {
             if(item.clearPrev){
                 userTextSpan.textContent=""
